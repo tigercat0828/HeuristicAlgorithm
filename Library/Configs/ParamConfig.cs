b2da7e921de2b1b5eaa407ad490ac33f3bc1f6e4
Tigercat <tigercat0828@gmail.com>
2025-08-31 19:32:59 +0800

    update

--- Library/Configs/ParamConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Library.Configs;
-public struct ParamConfig(int generations, int populations, double mutationRate) {
-    public int generations = generations;
-    public int populations = populations;
-    public double mutationRate = mutationRate;
-}
--- Temp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Library.Solvers;
-using System.Text.Json;
-
-Console.WriteLine("");
-
-JobSche tmep = new([1, 2, 3, 4], 100);
-JsonSerializerOptions options = new() { WriteIndented = true };
-string jsonString = JsonSerializer.Serialize(tmep, options);
-File.WriteAllText("test.json", jsonString);
